a78d9b43db888232b0b0a8bdc47805a051c0136e
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-02-18 12:58:26 +0530

    Create String Input.java

---
+++ Input & Output/String Input.java
@@ -0,0 +1,26 @@
+// Description:
+// Write a program to get the string as input and print it. When the user enters a string (i.e) char array, it is stored in variable testString.
+
+// Sample Input 1:
+// Programmer
+
+// Sample Output 1:
+// Programmer
+
+// Sample Input 2:
+// 897
+
+// Sample Output 2:
+// 897
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+      Scanner ar = new Scanner(System.in);
+      String str = ar.nextLine();
+      //System.out.println(str);
+      System.out.printf("%s\n", str);
+    }
+}
