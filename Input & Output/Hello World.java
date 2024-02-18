b57e15ac3adb1c53b2291922cbd43c215b6b07bc
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-02-18 12:41:30 +0530

    Update Hello World.java

--- Input & Output/Hello World.java
+++ Input & Output/Hello World.java
@@ -6,8 +6,8 @@ class Main
 {
   	public static void main(String args[])
   	{
-    	System.out.println("Hello World"); //Output: Hello World
+      System.out.println("Hello World"); //Output: Hello World
       System.out.println("Hello World\tHello World"); //Output: Hello World    Hello World (Tab)
       System.out.println("Hello World\nHello World"); //Output: Hello World \n Hello World (New line)
-  	}
+    }
 }
