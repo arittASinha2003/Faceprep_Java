0df5f240b4fb9114fb9b99da8510c9d14b07b9ca
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-02-18 12:31:48 +0530

    Create Hello World.java

---
+++ Input & Output/Hello World.java
@@ -0,0 +1,12 @@
+// Description:
+// Write a program to print "Hello World" and "Hello World    Hello World", ie, with tab.
+
+import java.util.Scanner;
+class Main
+{
+  	public static void main(String args[])
+  	{
+    	System.out.println("Hello World"); //Output: Hello World
+      System.out.println("Hello World\tHello World"); //Output: Hello World    Hello World
+  	}
+}
