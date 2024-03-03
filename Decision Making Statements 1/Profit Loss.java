ca4a624d864e05b9bdb24574475b3f8c6ea464fb
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-03-03 09:28:51 +0530

    Create Profit Loss.java

---
+++ Decision Making Statements 1/Profit Loss.java
@@ -0,0 +1,45 @@
+// Description?:
+// A fruit seller buys a dozen of banana at Rs.X. He sells 1 banana at Rs.Y. Write a java program to determine the profit or loss in Rs. for the fruitseller.
+// Input Format:
+// Input consists of 2 floating point numbers
+// The first input corresponds to the total cost(X)
+// The second input corresponds to the sold cost(Y)
+
+// Output Format:
+// Print "Profit or Loss" with Rupees.
+
+// Sample Input:
+// 60
+// 4
+
+// Sample Output:
+// Loss : Rs.12.00
+
+// Input (stdin):
+// 60.0
+// 5
+
+// Output (stdout):
+// No profit nor loss
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ar = new Scanner(System.in);
+        double X = ar.nextDouble();
+        double Y = ar.nextDouble();
+        double costPrice = X / 12;
+        double profitOrLoss = Y - costPrice;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        if (profitOrLoss > 0) {
+            System.out.println("Profit : Rs." + df.format(profitOrLoss * 12));
+        } else if (profitOrLoss < 0) {
+            System.out.println("Loss : Rs." + df.format(-profitOrLoss * 12));
+        } else {
+            System.out.println("No profit nor loss");
+        }
+    }
+}
