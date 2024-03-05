c316a6f3461029e04711fb4284e43ae916f1d36b
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-03-05 09:59:19 +0530

    Update Lab Allocation 4.java

--- Decision Making Statements 1/Lab Allocation 4.java
+++ Decision Making Statements 1/Lab Allocation 4.java
@@ -40,28 +40,77 @@
 // Output (stdout):
 // L2
 
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner ar = new Scanner(System.in);
+//         int L1 = ar.nextInt();
+//         int L2 = ar.nextInt();
+//         int L3 = ar.nextInt();
+//         int n = ar.nextInt();
+        
+//         int unusedL1 = L1 >= n ? L1 - n : Integer.MAX_VALUE;
+//         int unusedL2 = L2 >= n ? L2 - n : Integer.MAX_VALUE;
+//         int unusedL3 = L3 >= n ? L3 - n : Integer.MAX_VALUE;
+        
+//         if (unusedL1 <= Math.min(unusedL2, unusedL3)) {
+//             System.out.println("L1");
+//         } else if (unusedL2 <= Math.min(unusedL1, unusedL3)) {
+//             System.out.println("L2");
+//         } else if (unusedL3 <= Math.min(unusedL1, unusedL2)) {
+//             System.out.println("L3");
+//         } else {
+//             System.out.println("No lab can accommodate the given number of students");
+//         }
+//     }
+// }
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner ar = new Scanner(System.in);
+        
         int L1 = ar.nextInt();
         int L2 = ar.nextInt();
         int L3 = ar.nextInt();
         int n = ar.nextInt();
         
-        int unusedL1 = L1 >= n ? L1 - n : Integer.MAX_VALUE;
-        int unusedL2 = L2 >= n ? L2 - n : Integer.MAX_VALUE;
-        int unusedL3 = L3 >= n ? L3 - n : Integer.MAX_VALUE;
-        
-        if (unusedL1 <= Math.min(unusedL2, unusedL3)) {
+        if (n < L1 && n < L2 && n < L3) {
+            if (L1 < L2 && L1 < L3) {
+                System.out.println("L1");
+            } else if (L2 < L3) {
+                System.out.println("L2");
+            } else {
+                System.out.println("L3");
+            }
+        } else if (n < L1 && n < L2) {
+            if (L1 < L2) {
+                System.out.println("L1");
+            } else {
+                System.out.println("L2");
+            }
+        } else if (n < L1 && n < L3) {
+            if (L1 < L3) {
+                System.out.println("L1");
+            } else {
+                System.out.println("L3");
+            }
+        } else if (n < L2 && n < L3) {
+            if (L2 < L3) {
+                System.out.println("L2");
+            } else {
+                System.out.println("L3");
+            }
+        } else if (n < L1) {
             System.out.println("L1");
-        } else if (unusedL2 <= Math.min(unusedL1, unusedL3)) {
+        } else if (n < L2) {
             System.out.println("L2");
-        } else if (unusedL3 <= Math.min(unusedL1, unusedL2)) {
+        } else if (n < L3) {
             System.out.println("L3");
         } else {
-            System.out.println("No lab can accommodate the given number of students");
+            System.out.println("0");
         }
     }
 }
