4392bfb7b75104775140d9600e46389e14fa25a1
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-03-15 04:44:05 +0530

    Create Sum.java

---
+++ Daily Test/27/Sum.java
@@ -0,0 +1,41 @@
+// Description:
+// Write a Java Program to print addition of N numbers.
+// Given N integer numbers (user will read numbers one by one) and find sum/addition of all given numbers.
+
+// Input Format:
+// Input consist of an Integer
+
+// Output Format:
+// Output consist of an Integer
+
+// Sample Input:
+// 5
+// 100
+// 200
+// 300
+// 400
+// 500
+
+// Sample Output:
+// 1500
+
+// Input:
+// 5 456 34 23 56 34
+
+// Output:
+// 603
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ar = new Scanner(System.in);
+        int N = ar.nextInt();
+        int sum = 0;
+        for(int i = 0; i < N; i++) {
+            int num = ar.nextInt();
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
