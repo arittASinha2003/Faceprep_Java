016a08de15932dc4dc95ac4da6105476ceb896d7
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-03-16 02:53:36 +0530

    Create Word Repeat.java

---
+++ Daily Test/28/Word Repeat.java
@@ -0,0 +1,44 @@
+// Description:
+// Gowri is doing her homework. She needs to write a paragraph about Modern History. During that time, she noticed that some words are get repeated again and again. She started counting the number of times a particular word gets repeated. Write a java program to get a string from the user. Count the number of times a word gets repeated in the string.
+
+// Input Format:
+// The first line consists of a string
+// The second line consists of a string
+
+// Output Format:
+// Refer sample output for formatting specifications
+
+// Sample Input:
+// I felt happy because I saw the others were happy and because I knew I should feel happy
+// happy
+
+// Sample Output:
+// 3
+
+// Input:
+// Nory was a catholic because her mother was a catholic
+// catholic
+
+// Output:
+// 2
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ar = new Scanner(System.in);
+        String sentence = ar.nextLine();
+        String word = ar.nextLine();
+
+        String[] words = sentence.split("\\s+");
+        int count = 0;
+
+        for (String w : words) {
+            if (w.equals(word)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
